a573607eb6c139f35e54ed6904e04751844e0cdf
Sons123 <100420581+Sons123@users.noreply.github.com>
2023-03-05 15:06:07 +0000

    Edit the Update function

--- Assets/HelloWord.cs
+++ Assets/HelloWord.cs
@@ -7,12 +7,12 @@ public class HelloWord : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Hello World");
     }
 }
